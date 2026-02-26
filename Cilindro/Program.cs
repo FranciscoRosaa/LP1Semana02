653fba8f84ad405da444b57bd650e1de3b1512ed
Francisco <franciscorosa.work@gmail.com>
2026-02-26 15:56:50 +0000

    add Volume and Area

---
+++ Cilindro/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Cilindro
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Insere a altura do cilindro: ");
+            string altura = Console.ReadLine();
+            double a = double.Parse(altura);
+            Console.Write("Insere o raio do cilindro: ");
+            string raio = Console.ReadLine();
+            double r = double.Parse(raio);
+            double volume = Math.PI * Math.Pow(r, 2) * a;
+            double area = 2 * Math.PI * r * (r + a);
+            Console.WriteLine($"Volume = {Math.Round(volume, 3)}");
+            Console.WriteLine($"Área = {Math.Round(area, 3)}");
+        }
+    }
+}
