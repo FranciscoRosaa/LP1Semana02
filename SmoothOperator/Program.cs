71e2efe1d4c350b900d227d1228f203cfa1b3e48
Francisco <franciscorosa.work@gmail.com>
2026-02-26 12:41:44 +0000

    add SmoothOperator

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Insere um número inteiro: ");
+           string number = Console.ReadLine();
+           sbyte n = sbyte.Parse(number);
+           Console.WriteLine(--n);
+           Console.WriteLine(++n);
+        }
+    }
+}
