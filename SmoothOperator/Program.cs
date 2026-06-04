30efe6377eebcdb4c221109a2532aab0822615b3
Francisco <franciscorosa.work@gmail.com>
2026-06-04 17:13:54 +0100

    Add User input and increment/decrement of their number

--- SmoothOperator/Program.cs
+++ SmoothOperator/Program.cs
@@ -3,15 +3,24 @@ using System.Globalization;
 
 namespace SmoothOperator
 {
+    /// <summary>
+    /// Initializes the Program.
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Asks the user for a integrer number, converts the number into an
+        /// sbyte and decrements/increments that same number.
+        /// </summary>
+        /// <param name="args">Not Used.</param>
         private static void Main(string[] args)
         {
-            Console.Write("Insere um número inteiro: ");
-           string number = Console.ReadLine();
-           sbyte n = sbyte.Parse(number);
-           Console.WriteLine(--n);
-           Console.WriteLine(++n);
+            Console.Write("Insert an integrer number: ");
+            string number = Console.ReadLine();
+            sbyte snumber = sbyte.Parse(number);
+
+            Console.WriteLine($"Decremented number: {--snumber}");
+            Console.WriteLine($"Incremented number: {++snumber}");
         }
     }
 }
