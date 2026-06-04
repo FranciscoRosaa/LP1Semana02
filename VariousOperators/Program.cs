7f75e4770e821540b9371e0fbe07d421f82c9dde
Francisco <franciscorosa.work@gmail.com>
2026-06-04 17:37:35 +0100

    Add promt for user to give an non-negative number, that gets converted into a byte and gets put into 4 different math operations

--- VariousOperators/Program.cs
+++ VariousOperators/Program.cs
@@ -2,17 +2,28 @@
 
 namespace VariousOperators
 {
+    /// <summary>
+    /// Initializes the Program.
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Asks the user for an non-negative int number, stores and converts
+        /// it in a variable byte and divides it, shifts its bytes to the left
+        /// by 3, does a xor with 6 and sees if the number is higher or equal
+        /// than 10.
+        /// </summary>
+        /// <param name="args">Not used.</param>
         private static void Main(string[] args)
         {
-            Console.Write("Insere um número inteiro-não negativo: ");
+            Console.Write("Insert and non-negative int number: ");
             string number = Console.ReadLine();
-            byte n = byte.Parse(number);
-            Console.WriteLine(n / 2);
-            Console.WriteLine(n << 3);
-            Console.WriteLine(n ^ 6);
-            Console.WriteLine(n > 10);
+            byte bnumber = byte.Parse(number);
+
+            Console.WriteLine($"{bnumber} /2 = {bnumber / 2}");
+            Console.WriteLine($"{bnumber} << 3 = {bnumber << 3}");
+            Console.WriteLine($"{bnumber} ^ 6 = {bnumber ^ 6}");
+            Console.WriteLine($"{bnumber} >= 10 = {bnumber >= 10}");
         }
     }
 }
